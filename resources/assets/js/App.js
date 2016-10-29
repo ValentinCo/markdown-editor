4635e8c34afd8d1b132249e36baab4b6eb749cac
jordan <jordan.lefort@laposte.net>
2016-10-29 14:08:25 +0200

    onMouseOver and onMouseOut, test after click on item

--- resources/assets/js/App.js
+++ resources/assets/js/App.js
@@ -5,30 +5,33 @@ class App extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			active : false
+			active : false,
 		};
 
 		
 	}
 
 	updateDropDown(menu){
-		if(!this.state.active){
 
-			$(menu).addClass('transition visible');
-			this.state.active = !this.state.active;
-			
-		}else{
-
-			$(menu).removeClass('transition visible');
-			this.state.active = !this.state.active;
-		}
-
+		$(menu).addClass('transition visible');
+		$(menu).removeClass('lala');
+		$('.lala').removeClass('transition visible')
+		$(menu).addClass('lala');
+		
 	}
+
+	close(){
+
+		$('.lala').removeClass('transition visible')
+	}
+
+	
 
 	render(){
 		return(
 			<div>
-			<MenuFile onClick={this.updateDropDown.bind(this,'#menuFiles')}/>
+			<MenuFile onMouseOut={this.close} onMouseOver={this.updateDropDown.bind(this, '#menuFiles')}/>
+			<MenuHelp onMouseOut={this.close} onMouseOver={this.updateDropDown.bind(this, '#menuHelp')}/>
 			</div>
 		)
 	}
@@ -57,10 +60,10 @@ class IconeMenu extends React.Component{
 class MenuFile extends React.Component{
 	render(){
 		return(
-			<div onClick={this.props.onClick} className='ui dropdown'>
+			<div onMouseOut={this.props.onMouseOut} onMouseOver={this.props.onMouseOver} className='ui dropdown'>
 				<MenuTitle>Files</MenuTitle>		
 				<IconeMenu/>
-				<MenuFileContent id='menuFiles' >New Files</MenuFileContent>
+				<MenuFileContent  id='menuFiles'></MenuFileContent>
 			</div>
 		)
 	}
@@ -70,7 +73,7 @@ class MenuFile extends React.Component{
 class MenuFileContent extends React.Component{
 	render(){
 		return(
-			<div id={this.props.id} className='menu'>
+			<div id={this.props.id} className='menu lala'>
 				<div className="item">New Files</div>
 	 			<div className="item">
 	 				<span className="description">ctrl + o</span>
@@ -85,5 +88,27 @@ class MenuFileContent extends React.Component{
 	}
 }
 
+class MenuHelp extends React.Component{
+	render(){
+		return(
+			<div onMouseOut={this.props.onMouseOut} onMouseOver={this.props.onMouseOver} className='ui dropdown'>
+				<MenuTitle>Help</MenuTitle>		
+				<IconeMenu/>
+				<MenuHelpContent id='menuHelp' ></MenuHelpContent>
+			</div>
+		)
+	}
+}
+
+class MenuHelpContent extends React.Component{
+	render(){
+		return(
+			<div id={this.props.id} className='menu lala'>
+				<div className="item">Info ?</div>
+	 			<div className="item">Learn Markdown</div>
+			</div>
+		)
+	}
+}
 
 export default App;
