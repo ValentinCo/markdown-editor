43a1360da37be7129689739e67d91928cf065a4f
jordan <jordan.lefort@laposte.net>
2016-10-30 09:25:08 +0100

    add save current file

--- js/save.js
+++ js/save.js
@@ -3,7 +3,37 @@ var dialog = app.dialog;
 var fs = require('fs');
 var content;
 
+$('#save').on('click',function(){
+	content = $('.setMarkdownTextarea').val();
+	if(window.fileName === undefined){
 
+		dialog.showSaveDialog(function (fileName) {
+	      
+	       if (fileName === undefined){
+	            alert("You didn't save the file");
+	            return;
+	        }
+	        
+	       fs.writeFile(fileName, content, function (err) {
+	           if(err){
+	               alert("An error ocurred creating the file "+ err.message)
+	           }
+	                        
+	           alert("The file has been saved");
+	       });
+		}); 
+		return;
+	}
+
+	fs.writeFile(window.fileName, content, function (err) {
+        if(err){
+            alert("An error ocurred creating the file "+ err.message)
+        }
+                        
+        alert("The file has been saved");
+    });
+
+});
 
 
 $('#saveAs').on('click',function(){
@@ -26,5 +56,6 @@ $('#saveAs').on('click',function(){
 	}); 
 		
 })
+
 
 
