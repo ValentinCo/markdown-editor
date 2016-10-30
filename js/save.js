22bac2f0bdc060b74e43830a0a2fde0fe54a50c5
jordan <jordan.lefort@laposte.net>
2016-10-30 09:51:35 +0100

    add eventKey listener

--- js/save.js
+++ js/save.js
@@ -2,8 +2,33 @@ var app = require('electron').remote;
 var dialog = app.dialog;
 var fs = require('fs');
 var content;
+var listener = new window.keypress.Listener();
 
-$('#save').on('click',function(){
+
+function saveAs(){
+	content = $('.setMarkdownTextarea').val();
+
+	dialog.showSaveDialog(function (fileName) {
+        if (fileName === undefined){
+           alert("You didn't save the file");
+           return;
+        }
+
+        window.fileName = fileName;
+        console.log(window.fileName)
+
+        fs.writeFile(fileName, content, function (err) {
+           if(err){
+               alert("An error ocurred creating the file "+ err.message)
+           }
+                        
+           alert("The file has been saved");
+           return;
+       });
+	}); 
+}
+
+function save(){
 	content = $('.setMarkdownTextarea').val();
 	if(window.fileName === undefined){
 
@@ -32,30 +57,22 @@ $('#save').on('click',function(){
                         
         alert("The file has been saved");
     });
+}
 
-});
 
+listener.simple_combo("ctrl shift s", saveAs.bind(this));
 
 $('#saveAs').on('click',function(){
-
-	content = $('.setMarkdownTextarea').val();
-
-	dialog.showSaveDialog(function (fileName) {
-       if (fileName === undefined){
-            alert("You didn't save the file");
-            return;
-      }
-        
-       fs.writeFile(fileName, content, function (err) {
-           if(err){
-               alert("An error ocurred creating the file "+ err.message)
-           }
-                        
-           alert("The file has been saved");
-       });
-	}); 
-		
+	saveAs();	
 })
+
+
+
+listener.simple_combo("ctrl s", save.bind(this));
+
+$('#save').on('click',function(){
+	save();
+});
 
 
 
