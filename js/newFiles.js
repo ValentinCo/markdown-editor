e4bb4c7fbae450dc6263acf97c99a896b753e1f1
jordan <jordan.lefort@laposte.net>
2016-11-02 07:38:02 +0100

    refactorisation ok;

--- js/newFiles.js
+++ js/newFiles.js
@@ -1,72 +1,83 @@
-var app = require('electron').remote; 
-var dialog = app.dialog;
-var fs = require('fs');
-var buttons = ['OK', 'Save and create new file', 'Cancel'];
+(function(){
+	var app = require('electron').remote; 
+	var dialog = app.dialog;
+	var fs = require('fs');
+	var data = require('./js/data.js');
 
-$('#newFile').on('click',function(){
-	if(window.itIsSaved){
-		$('.setMarkdownTextarea').val('');
-		$('.viewMarkdown').html('');
-		window.filePath = undefined;
-		return;
-	}
-
-dialog.showMessageBox({ type: 'info', buttons: buttons, message: 'Create new file without saving your last work ?' }, function (response) {
-    	
-    	if(response === 0){
-    		$('.setMarkdownTextarea').val('');
-			$('.viewMarkdown').html('');
-			window.filePath = undefined;
-			return;
-    	}
-
-    	if(response === 2){
-    		return;
-    	}
-
-    	content = $('.setMarkdownTextarea').val();
+	var apply = {
+		buttons : ['Create new file', 'Save and create new file', 'Cancel'],
+		message : 'Create new file without saving your last work ?',
 		
-		if(window.filePath === undefined){
+		init(){
+			this.listerner();
+		},
 
-			dialog.showSaveDialog(function (fileName) {
-			      
+		listerner(){
+			$('#newFile').on('click',apply.newFile)
+		},
+
+		isSaved(){
+			$('.setMarkdownTextarea').val('');
+			$('.viewMarkdown').html('');
+			$('title').html('Markdown Editor /new file')
+			data.filePath = undefined;
+			data.itIsSaved = true;
+		},
+
+		content(){
+			data.content = $('.setMarkdownTextarea').val();
+		},
+
+		showSaveDialog(){
+			dialog.showSaveDialog(function (fileName) {     
 		       if (fileName === undefined){
 		            alert("You didn't save the file");
 		            return;
 		        }
-		        
-		       fs.writeFile(fileName, content, function (err) {
-		           if(err){
-		               alert("An error ocurred creating the file "+ err.message)
-		           }
-		            window.itIsSaved = true;         
-		            alert("The file has been saved");
-		            $('.setMarkdownTextarea').val('');
-				    $('.viewMarkdown').html('');
-					window.filePath = undefined;
-					return;
-				});
+		        apply.writeFile(fileName);
 			}); 
-			return;
+		},
+
+		writeFile(fileName){
+			fs.writeFile(fileName, data.content, function (err) {
+	            if(err){
+	               alert("An error ocurred creating the file "+ err.message)
+	            }
+	            alert("The file has been saved");
+	            apply.isSaved();
+			});
+		},
+
+		save(){
+			if(data.filePath === undefined){
+				apply.showSaveDialog();
+				return;
+			}
+			apply.writeFile(data.filePath);
+		},
+
+		newFile(){
+			if(data.itIsSaved){
+				apply.isSaved();
+				return;
+			}
+			apply.content();
+			dialog.showMessageBox({ type: 'info', buttons: apply.buttons, message: apply.message }, function (response) {
+
+		   		if(response === 0){
+		    		apply.isSaved();
+					return;
+		    	}
+		    	if(response === 2){
+		    		return;
+		    	}
+		    	apply.save();
+		 	});
+
 		}
+	}
+	apply.init();
+})();
 
-		fs.writeFile(window.filePath, content, function (err) {
-	        
-	        if(err){
-	            alert("An error ocurred creating the file "+ err.message)
-	        }
-
-	        window.itIsSaved = true;               
-	        alert("The file has been saved");
-
-	        $('.setMarkdownTextarea').val('');
-			$('.viewMarkdown').html('');
-			window.filePath = undefined;
-			return;
-
-   		 });
-
- 	});
-});
 
 
